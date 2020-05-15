e57c78aa85b133d01b58f6baaa6f8bea38c3b9da
melpon <shigemasa7watanabe+github@gmail.com>
2020-05-16 03:08:29 +0900

    ひとまず aar は Android パッケージに含めない

--- scripts/package_webrtc_android.sh
+++ scripts/package_webrtc_android.sh
@@ -15,7 +15,7 @@ VERSION_FILE=$5
 
 rm -rf $BUILD_DIR/package/webrtc
 mkdir -p $BUILD_DIR/package/webrtc/lib
-mkdir -p $BUILD_DIR/package/webrtc/aar
+# mkdir -p $BUILD_DIR/package/webrtc/aar
 mkdir -p $BUILD_DIR/package/webrtc/jar
 mkdir -p $BUILD_DIR/package/webrtc/include
 
@@ -23,8 +23,8 @@ mkdir -p $BUILD_DIR/package/webrtc/include
 rsync -amv '--include=*/' '--include=*.h' '--include=*.hpp' '--exclude=*' $SOURCE_DIR/webrtc/src/. $BUILD_DIR/package/webrtc/include/.
 
 # libwebrtc.aar, libwebrtc.a, NOTICE
-cp $BUILD_DIR/webrtc_android/libwebrtc.aar $BUILD_DIR/package/webrtc/aar/
-cp $BUILD_DIR/webrtc_android/libwebrtc_onremovetrack.aar $BUILD_DIR/package/webrtc/aar/
+# cp $BUILD_DIR/webrtc_android/libwebrtc.aar $BUILD_DIR/package/webrtc/aar/
+# cp $BUILD_DIR/webrtc_android/libwebrtc_onremovetrack.aar $BUILD_DIR/package/webrtc/aar/
 mkdir -p $BUILD_DIR/package/webrtc/lib/armeabi-v7a/
 cp $BUILD_DIR/webrtc_armeabi-v7a/libwebrtc.a $BUILD_DIR/package/webrtc/lib/armeabi-v7a/
 mkdir -p $BUILD_DIR/package/webrtc/lib/arm64-v8a/
@@ -34,7 +34,7 @@ cp $BUILD_DIR/webrtc_android/LICENSE.md $BUILD_DIR/package/webrtc/NOTICE
 # libwebrtc.aar を unzip して classes.jar を配置する
 mkdir -p $BUILD_DIR/package/webrtc/jar/tmp
 pushd $BUILD_DIR/package/webrtc/jar/tmp
-  unzip $BUILD_DIR/package/webrtc/aar/libwebrtc.aar
+  unzip $BUILD_DIR/webrtc_android/libwebrtc.aar
   mv classes.jar ../webrtc.jar
 popd
 rm -r $BUILD_DIR/package/webrtc/jar/tmp
