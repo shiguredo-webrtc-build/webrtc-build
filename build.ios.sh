24f182e14a0cb3a7fea667ff8509fb9e79d632d5
enm10k <enm10k@gmail.com>
2021-07-28 21:34:39 +0900

    release ビルドでも dSYM が必要になった

--- build.ios.sh
+++ build.ios.sh
@@ -55,7 +55,6 @@ pushd $SOURCE_DIR/webrtc/src
       rtc_use_h264=false \
       use_rtti=true \
       libcxx_abi_unstable=false \
-      enable_dsyms=$_is_debug \
     "
     _branch="M`echo $WEBRTC_VERSION | cut -d'.' -f1`"
     _commit="`echo $WEBRTC_VERSION | cut -d'.' -f3`"
