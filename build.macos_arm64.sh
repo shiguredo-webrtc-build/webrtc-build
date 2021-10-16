5c20005495c03860b3ca06d1aa4ce2d23af2ad1a
soudegesu <soudegesu@hotmail.co.jp>
2021-10-16 14:18:01 +0900

    ローカルデバッグしやすいように古い成果物を削除する

--- build.macos_arm64.sh
+++ build.macos_arm64.sh
@@ -102,6 +102,7 @@ EOF
     python2 tools_webrtc/libs/generate_licenses.py --target //sdk:mac_framework_objc $_libs_dir/WebRTC.framework/Resources $_libs_dir
 
     # xcframeworkの作成
+    rm -rf $BUILD_DIR/webrtc/${_build_config}/WebRTC.xcframework
     xcodebuild -create-xcframework \
           -framework $BUILD_DIR/webrtc/${_build_config}/WebRTC.framework -debug-symbols $BUILD_DIR/webrtc/$_build_config/WebRTC.dSYM \
           -output $BUILD_DIR/webrtc/${_build_config}/WebRTC.xcframework
