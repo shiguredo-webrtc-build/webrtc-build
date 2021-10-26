8e7e40309569163b1523dc5a56a9aa652a36620c
melpon <shigemasa7watanabe+github@gmail.com>
2021-10-26 14:29:21 +0900

    修正

--- run.py
+++ run.py
@@ -494,6 +494,8 @@ def build_webrtc_android(source_dir, build_dir, version_info: VersionInfo, debug
     gn_args_base = [
         f"is_debug={'true' if debug else 'false'}",
         f"is_java_debug={'true' if debug else 'false'}",
+        # Docker 上でビルドするとなぜか警告が出るので無効化する
+        'treat_warnings_as_errors=false',
         *COMMON_GN_ARGS
     ]
 
