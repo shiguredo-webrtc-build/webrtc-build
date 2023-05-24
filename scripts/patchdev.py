d9166110fa80aa023637a4e47d50303388912a09
SUZUKI Tetsuya <tetsuya.suzuki@gmail.com>
2023-05-24 16:27:02 +0900

    全体的に書き直し

--- scripts/patchdev.py
+++ scripts/patchdev.py
@@ -7,23 +7,52 @@ import sys
 import os
 import json
 
-base_dir = 'patchdev'
-work_dir = os.getcwd()
+# トップレベルのパス
+top_dir = os.path.normpath(os.path.join(os.path.abspath(os.path.dirname(__file__)), '..'))
+patchdev_dir = os.path.join(top_dir, 'patchdev')
+
+
+def init_project_with_cur_dir():
+    init_project(os.path.basename(os.getcwd()))
+
+
+def init_project(name):
+    global project_dir, project_src_dir, project_build_dir, project_build_patches_dir, config_file
+    project_dir = os.path.join(patchdev_dir, name)
+    project_src_dir = os.path.join(project_dir, 'src')
+    project_build_dir = os.path.join(project_dir, '_build')
+    project_build_patches_dir = os.path.join(project_build_dir, 'patches')
+    config_file = os.path.join(project_dir, "config.json")
 
 
 def rtc_src_dir(platform):
-    return os.path.join("..", "..", "_source", platform, "webrtc", "src")
+    return os.path.join(top_dir, "_source", platform, "webrtc", "src")
 
 
 def rtc_src_file(platform, source):
     return os.path.join(rtc_src_dir(platform), source)
 
 
+def load_config():
+    with open(config_file) as f:
+        return Config(json.load(f))
+
+
+class Config:
+    def __init__(self, json):
+        self.output = json["output"]
+        self.platform = json["platform"]
+        self.build_flags = json["build_flags"]
+        self.sources = []
+        for path in json["sources"]:
+            self.sources.append(os.path.normpath(path))
+
+
 def init(args):
     project_name = args.project_name
-    project_dir = os.path.join(base_dir, project_name)
+    init_project(project_name)
     os.makedirs(project_dir, exist_ok=True)
-    os.makedirs(os.path.join(project_dir, "src"), exist_ok=True)
+    os.makedirs(project_src_dir, exist_ok=True)
 
     # Makefile
     makefile_content = """
@@ -64,116 +93,92 @@ clean:
         "build_flags": "--debug",
         "sources": [],
     }
-    with open(os.path.join(project_dir, "config.json"), 'w') as f:
+    with open(config_file, 'w') as f:
         json.dump(config_content, f, indent=4)
 
     print("Initialized project in:", project_dir)
 
 
 def build(args):
-    with open("config.json") as f:
-        config = json.load(f)
+    config = load_config()
+    check_all_files(config.platform, config.sources)
 
-    platform = config["platform"]
-    sources = config["sources"]
-    check_all_files(sources)
-
-    for source in sources:
-        shutil.copy2(f"src/{source}", rtc_src_file(platform, source))
+    for source in config.sources:
+        shutil.copy2(os.path.join(project_src_dir, source), rtc_src_file(config.platform, source))
 
     orig_dir = os.getcwd()
-    os.chdir("../../")
-    build_flags = config["build_flags"]
-    cmd = f"python3 run.py build {platform} {build_flags}\n"
+    os.chdir(top_dir)
+    cmd = f"python3 run.py build {config.platform} {config.build_flags}\n"
     print(f"exec: {cmd}")
     os.system(cmd)
     os.chdir(orig_dir)
 
 
 def generate(args):
-    # config.jsonをロード
-    with open("config.json") as f:
-        config = json.load(f)
-
-    # platformの値を取得
-    platform = config["platform"]
-    sources = config["sources"]
-    check_all_files(sources)
+    config = load_config()
+    check_all_files(config.platform, config.sources)
 
     # パッチファイルのリスト
     patch_files = []
 
     # 各ソースファイルに対して操作を実行
-    for source in sources:
-        target = rtc_src_file(platform, source)
-        shutil.copy2(os.path.join('src', source), target)
+    for source in config.sources:
+        target = rtc_src_file(config.platform, source)
+        shutil.copy2(os.path.join(project_src_dir, source), target)
         os.chdir(os.path.dirname(target))
         cmd = ['git', 'add', '-N', os.path.basename(target)]
         subprocess.check_output(cmd)
         cmd = ['git', 'diff', os.path.basename(target)]
         diff = subprocess.check_output(cmd).decode('utf-8')
-        os.chdir(work_dir)
-        patch_file = os.path.join('_build/patches', f'{source}.patch')
+        os.chdir(project_dir)
+        patch_file = os.path.join(project_build_patches_dir, f'{source}.patch')
         os.makedirs(os.path.dirname(patch_file), exist_ok=True)
         with open(patch_file, 'w') as f:
             f.write(diff)
         patch_files.append(patch_file)
 
     # パッチファイルを結合
-    with open(os.path.join('_build', config["output"]), 'w') as outfile:
+    with open(os.path.join(project_build_dir, config.output), 'w') as outfile:
         for patch_file in patch_files:
             with open(patch_file) as infile:
                 outfile.write(infile.read())
 
 
 def clean(args):
-    shutil.rmtree('_build', ignore_errors=True)
+    config = load_config()
 
-    with open("config.json") as f:
-        config = json.load(f)
+    shutil.rmtree(project_build_dir, ignore_errors=True)
 
-    platform = config["platform"]
-    for source in config["sources"]:
-        source_path = rtc_src_file(platform, source)
+    for source in config.sources:
+        source_path = rtc_src_file(config.platform, config.source)
         os.chdir(os.path.dirname(source_path))
         os.system(f"git checkout -- {os.path.basename(source_path)}")
 
 
 def diff(args):
-    # config.jsonをロード
-    with open("config.json") as f:
-        config = json.load(f)
+    config = load_config()
 
-    # platformの値を取得
-    platform = config["platform"]
-    sources = config["sources"]
-    check_all_files(sources)
+    check_all_files(config.platform, config.sources)
 
-    for source in sources:
-        target = rtc_src_file(platform, source)
-        shutil.copy2(os.path.join('src', source), target)
+    for source in config.sources:
+        target = rtc_src_file(config.platform, source)
+        shutil.copy2(os.path.join(project_src_dir, source), target)
         os.chdir(os.path.dirname(target))
         cmd = ['git', 'add', '-N', os.path.basename(target)]
         subprocess.check_output(cmd)
         cmd = ['git', 'diff', os.path.basename(target)]
         diff = subprocess.check_output(cmd).decode('utf-8')
-        os.chdir(work_dir)
+        os.chdir(project_dir)
         print(diff)
 
 
 def sync(args):
-    patch_dir = os.getcwd()
-    config_path = os.path.join(patch_dir, "config.json")
+    config = load_config()
 
-    # config.json のロード
-    with open(config_path, "r") as json_file:
-        config = json.load(json_file)
-
-    platform = config['platform']
     copied_files = 0
-    for source in config['sources']:
+    for source in config.sources:
         # コピー先のファイルパス
-        destination_path_in_src = os.path.join(patch_dir, "src", source)
+        destination_path_in_src = os.path.join(project_src_dir, source)
 
         # コピー先ディレクトリの生成
         os.makedirs(os.path.dirname(destination_path_in_src), exist_ok=True)
@@ -181,18 +186,17 @@ def sync(args):
         # ファイルが存在しなければオリジナルからコピー
         if not os.path.isfile(destination_path_in_src):
             # パッチ対象ファイルのオリジナルのパス
-            original_path = rtc_src_file(platform, source)
+            original_path = rtc_src_file(config.platform, source)
             shutil.copy2(original_path, destination_path_in_src)
-            print(f"パッチ対象ファイルをコピーしました: {destination_path_in_src}")
+            print(f"Copied: {destination_path_in_src}")
             copied_files += 1
+    if copied_files == 0:
+        print("No files copied.")
 
 
 def check(args):
-    with open("config.json") as f:
-        config = json.load(f)
-
-    sources = config["sources"]
-    check_all_files(sources)
+    config = load_config()
+    check_all_files(config.platform, config.sources)
 
 
 def check_newline_at_eof(file_path):
@@ -201,28 +205,34 @@ def check_newline_at_eof(file_path):
         sys.exit(1)
 
     with open(file_path, 'rb') as f:
-        f.seek(-2, os.SEEK_END)
-        last_two_bytes = f.read(2)
+        f.seek(-1, os.SEEK_END)
+        last = f.read()
 
-    return last_two_bytes[-1:] != b'\n'
+    return last == b'\n'
 
 
-def check_all_files(sources):
+def check_all_files(platform, sources):
     has_error = False
     for source in sources:
-        file_path = os.path.join('src', source)
-        error = check_newline_at_eof(file_path)
+        file_path = rtc_src_file(platform, source)
+        print('Checking:', file_path)
+        error = not check_newline_at_eof(file_path)
         if error:
             print(f"Error: The file {file_path} does not end with a newline.")
-        has_error = has_error or error
+            has_error = True
     if has_error:
         sys.exit(1)
 
 
 def main():
+    init_project_with_cur_dir()
+
+    # カレントディレクトリをトップレベルに変更
+    os.chdir(top_dir)
+
     parser = argparse.ArgumentParser(
         description="パッチ開発用コマンドラインツール",
-        epilog="詳細については、PATCH_DEVELOP.mdを参照してください。"
+        epilog="詳細は PATCH_DEVELOP_TOOL.md を参照してください。"
     )
 
     subparsers = parser.add_subparsers(
