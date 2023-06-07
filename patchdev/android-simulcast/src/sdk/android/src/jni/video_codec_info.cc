74a403ea022b19c2d69010976780132f6c81654d
SUZUKI Tetsuya <tetsuya.suzuki@gmail.com>
2023-06-07 15:41:25 +0900

    VideoCodecInfo に scalabilityModes プロパティを追加する

--- patchdev/android-simulcast/src/sdk/android/src/jni/video_codec_info.cc
+++ patchdev/android-simulcast/src/sdk/android/src/jni/video_codec_info.cc
@@ -14,14 +14,22 @@
 #include "sdk/android/native_api/jni/java_types.h"
 #include "sdk/android/src/jni/jni_helpers.h"
 
+#include "absl/container/inlined_vector.h"
+
 namespace webrtc {
 namespace jni {
 
 SdpVideoFormat VideoCodecInfoToSdpVideoFormat(JNIEnv* jni,
                                               const JavaRef<jobject>& j_info) {
+  absl::InlinedVector<webrtc::ScalabilityMode, webrtc::kScalabilityModeCount> scalabilityModes;
+  std::vector<int32_t> javaScalabilityModes = JavaToNativeIntArray(jni, Java_VideoCodecInfo_getScalabilityModes(jni, j_info));
+  for (const auto& scalabilityMode : javaScalabilityModes) {
+    scalabilityModes.push_back(static_cast<webrtc::ScalabilityMode>(scalabilityMode));
+  }
   return SdpVideoFormat(
       JavaToNativeString(jni, Java_VideoCodecInfo_getName(jni, j_info)),
-      JavaToNativeStringMap(jni, Java_VideoCodecInfo_getParams(jni, j_info)));
+      JavaToNativeStringMap(jni, Java_VideoCodecInfo_getParams(jni, j_info)),
+      scalabilityModes);
 }
 
 ScopedJavaLocalRef<jobject> SdpVideoFormatToVideoCodecInfo(
@@ -29,8 +37,18 @@ ScopedJavaLocalRef<jobject> SdpVideoFormatToVideoCodecInfo(
     const SdpVideoFormat& format) {
   ScopedJavaLocalRef<jobject> j_params =
       NativeToJavaStringMap(jni, format.parameters);
-  return Java_VideoCodecInfo_Constructor(
+
+  ScopedJavaLocalRef<jobject> codec = Java_VideoCodecInfo_Constructor(
       jni, NativeToJavaString(jni, format.name), j_params);
+
+  size_t size = format.scalability_modes.size();
+  std::vector<int32_t> temp(size);
+  for (size_t i = 0; i < size; i++) {
+    temp[i] = static_cast<jint>(format.scalability_modes[i]);
+  }
+  Java_VideoCodecInfo_setScalabilityModes(jni, codec, NativeToJavaIntArray(jni, temp));
+
+  return codec;
 }
 
 }  // namespace jni
