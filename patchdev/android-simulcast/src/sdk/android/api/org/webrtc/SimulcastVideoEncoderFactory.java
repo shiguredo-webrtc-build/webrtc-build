e4b71ab87717327d60527d325321bde7ac24e2b9
SUZUKI Tetsuya <tetsuya.suzuki@gmail.com>
2023-06-19 15:11:44 +0900

    インデントを修正する

--- patchdev/android-simulcast/src/sdk/android/api/org/webrtc/SimulcastVideoEncoderFactory.java
+++ patchdev/android-simulcast/src/sdk/android/api/org/webrtc/SimulcastVideoEncoderFactory.java
@@ -8,44 +8,43 @@
  *  be found in the AUTHORS file in the root of the source tree.
  */
 
- package org.webrtc;
+package org.webrtc;
 
- import androidx.annotation.Nullable;
- import java.util.ArrayList;
- import java.util.HashMap;
- import java.util.List;
- import java.util.Arrays;
+import androidx.annotation.Nullable;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Arrays;
  
- public class SimulcastVideoEncoderFactory implements VideoEncoderFactory {
+public class SimulcastVideoEncoderFactory implements VideoEncoderFactory {
  
     static native List<VideoCodecInfo> nativeVP9Codecs();
     static native VideoCodecInfo nativeAV1Codec();
 
-     VideoEncoderFactory primary;
-     VideoEncoderFactory fallback;
+    VideoEncoderFactory primary;
+    VideoEncoderFactory fallback;
  
-     public SimulcastVideoEncoderFactory(VideoEncoderFactory primary, VideoEncoderFactory fallback) {
-         this.primary = primary;
-         this.fallback = fallback;
-     }
+    public SimulcastVideoEncoderFactory(VideoEncoderFactory primary, VideoEncoderFactory fallback) {
+        this.primary = primary;
+        this.fallback = fallback;
+    }
  
-     @Nullable
-     @Override
-     public VideoEncoder createEncoder(VideoCodecInfo info) {
-         return new SimulcastVideoEncoder(primary, fallback, info);
-     }
+    @Nullable
+    @Override
+    public VideoEncoder createEncoder(VideoCodecInfo info) {
+        return new SimulcastVideoEncoder(primary, fallback, info);
+    }
  
-     @Override
-     public VideoCodecInfo[] getSupportedCodecs() {
-         List<VideoCodecInfo> codecs = new ArrayList<VideoCodecInfo>();
-         codecs.addAll(Arrays.asList(primary.getSupportedCodecs()));
-         if (fallback != null) {
-             codecs.addAll(Arrays.asList(fallback.getSupportedCodecs()));
-         }
-         codecs.addAll(nativeVP9Codecs());
-         codecs.add(nativeAV1Codec());
-         return codecs.toArray(new VideoCodecInfo[codecs.size()]);
-     }
- 
- }
- 
+    @Override
+    public VideoCodecInfo[] getSupportedCodecs() {
+        List<VideoCodecInfo> codecs = new ArrayList<VideoCodecInfo>();
+        codecs.addAll(Arrays.asList(primary.getSupportedCodecs()));
+        if (fallback != null) {
+            codecs.addAll(Arrays.asList(fallback.getSupportedCodecs()));
+        }
+        codecs.addAll(nativeVP9Codecs());
+        codecs.add(nativeAV1Codec());
+        return codecs.toArray(new VideoCodecInfo[codecs.size()]);
+    }
+
+}
