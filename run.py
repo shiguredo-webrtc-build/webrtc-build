309ccb9381d25b7b5e73365b037c2dd5f1ba229c
melpon <shigemasa7watanabe+github@gmail.com>
2024-05-08 12:24:55 +0900

    Ubuntu 24.04 を試す

--- run.py
+++ run.py
@@ -304,6 +304,13 @@ PATCHES = {
         "ssl_verify_callback_with_native_handle.patch",
         "h265.patch",
     ],
+    "ubuntu-24.04_x86_64": [
+        "add_deps.patch",
+        "4k.patch",
+        "add_license_dav1d.patch",
+        "ssl_verify_callback_with_native_handle.patch",
+        "h265.patch",
+    ],
 }
 
 
@@ -839,7 +846,7 @@ def build_webrtc(
                     "arm_use_neon=false",
                     "enable_libaom=false",
                 ]
-        elif target in ("ubuntu-20.04_x86_64", "ubuntu-22.04_x86_64"):
+        elif target in ("ubuntu-20.04_x86_64", "ubuntu-22.04_x86_64", "ubuntu-24.04_x86_64"):
             gn_args += [
                 'target_os="linux"',
                 "rtc_use_pipewire=false",
@@ -1133,6 +1140,7 @@ TARGETS = [
     "macos_arm64",
     "ubuntu-20.04_x86_64",
     "ubuntu-22.04_x86_64",
+    "ubuntu-24.04_x86_64",
     "ubuntu-18.04_armv8",
     "ubuntu-20.04_armv8",
     "ubuntu-22.04_armv8",
@@ -1184,6 +1192,8 @@ def check_target(target):
         if target == "ubuntu-20.04_x86_64" and osver == "20.04":
             return True
         if target == "ubuntu-22.04_x86_64" and osver == "22.04":
+            return True
+        if target == "ubuntu-24.04_x86_64" and osver == "24.04":
             return True
 
         return False
