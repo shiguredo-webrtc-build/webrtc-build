c5c0d6b459df104f45b077605a4f4191fbca5068
melpon <shigemasa7watanabe+github@gmail.com>
2024-07-09 14:48:40 +0900

    復活したパッチを削除

--- run.py
+++ run.py
@@ -197,7 +197,6 @@ PATCHES = {
         "windows_fix_optional.patch",
         "windows_fix_audio_device.patch",
         "ssl_verify_callback_with_native_handle.patch",
-        "windows_fix_typo_in_deprecated_attribute.patch",
         "h265.patch",
         "fix_perfetto.patch",
     ],
