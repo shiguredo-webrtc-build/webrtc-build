2474ab250e2df0e8f074239c6b6b2895c48d1a27
voluntas <nakai@shiguredo.jp>
2026-09-25 12:15:10 +0900

    0031 AAR を Release のアーカイブから取り出すように変更する

--- scripts/prepare_aar.sh
+++ scripts/prepare_aar.sh
@@ -2,21 +2,22 @@
 
 set -eu
 
-# JitPack のビルド時に実行し、 Release に添付した libwebrtc.aar を
-# ローカルの Maven リポジトリに登録するスクリプト
+# JitPack のビルド時に実行し、 Release の webrtc.android_sdk.tar.gz から
+# libwebrtc.aar を取り出してローカルの Maven リポジトリに登録するスクリプト
 #
 # JitPack はビルド対象のタグ名を環境変数 VERSION に設定する (例: m155.8059.1.0)
 # 登録した artifact は com.github.shiguredo-webrtc-build:webrtc-build として公開される
 
-AAR_URL="https://github.com/shiguredo-webrtc-build/webrtc-build/releases/download/${VERSION}/libwebrtc.aar"
+SDK_URL="https://github.com/shiguredo-webrtc-build/webrtc-build/releases/download/${VERSION}/webrtc.android_sdk.tar.gz"
 
 echo "VERSION=${VERSION}"
-echo "AAR_URL=${AAR_URL}"
+echo "SDK_URL=${SDK_URL}"
 
-curl -fsSL -o libwebrtc.aar "${AAR_URL}"
+curl -fsSL -o webrtc.android_sdk.tar.gz "${SDK_URL}"
+tar -xzf webrtc.android_sdk.tar.gz webrtc/aar/libwebrtc.aar
 
 mvn install:install-file \
-    -Dfile=libwebrtc.aar \
+    -Dfile=webrtc/aar/libwebrtc.aar \
     -Dpackaging=aar \
     -Dversion="${VERSION}" \
     -DgroupId=com.github.shiguredo-webrtc-build \
